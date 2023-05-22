7ac60d2ccb84b6223a7a6e0e4c60a9b168fd7066
Alejandro <alejandro.dominguez-lopez@iesruizgijon.com>
2023-05-22 18:44:59 +0200

    Calculadora suma y resta

---
+++ src/calculadoragit/Metodos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoragit;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Metodos {
+  
+  public void suma(int x, int y) {
+    int suma = x + y;
+    System.out.println("La suma es igual a " + suma);
+  }
+
+  public void resta(int x, int y) {
+    int resta = x - y;
+    System.out.println("La resta es igual a " + resta);
+  }
+}
